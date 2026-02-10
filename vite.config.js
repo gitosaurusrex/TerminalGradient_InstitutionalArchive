55b1ac791127a1b7ba000fd40f257cd11d183f3c
gitosaurusrex <gitosaurusrex@users.noreply.github.com>
2026-02-09 20:59:32 -0500

    build: update vite config to include all html entry points

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
     server: {
@@ -8,6 +9,18 @@ export default defineConfig({
     assetsInclude: ['**/*.sql', '**/*.db', '**/*.wasm'],
     build: {
         outDir: 'dist',
-        assetsInlineLimit: 0 // Ensure large files are not inlined as base64
+        assetsInlineLimit: 0, // Ensure large files are not inlined as base64
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+                archive: resolve(__dirname, 'archive-index.html'),
+                caseIndex: resolve(__dirname, 'case-index.html'),
+                caseView: resolve(__dirname, 'case-view.html'),
+                fragmentIndex: resolve(__dirname, 'fragment-index.html'),
+                fragmentView: resolve(__dirname, 'fragment-view.html'),
+                search: resolve(__dirname, 'search.html'),
+            }
+        }
     }
 });
